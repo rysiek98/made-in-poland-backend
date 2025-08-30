a296eae46d06043dd1b450229c6e444f9a385514
michal.ryszka <m.ryszka98@gmail.com>
2025-08-30 20:01:56 +0200

    Initial commit

---
+++ src/main/java/pl/made_in_poland_backend/MadeInPolandBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.made_in_poland_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadeInPolandBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadeInPolandBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/made_in_poland_backend/MadeInPolandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.made_in_poland_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadeInPolandBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
